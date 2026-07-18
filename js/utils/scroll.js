89f3fdfcc73787685d5bd056ee803fbb1bbe8dad
Sherwin Degay Latawan <131456650+userweyn@users.noreply.github.com>
2026-07-18 15:20:13 +0800

    Update scroll.js

--- js/utils/scroll.js
+++ js/utils/scroll.js
@@ -1,25 +1,41 @@
-// ============================================
-// SCROLL REVEAL
-// ============================================
 function initScrollReveal(){
-    const observer = new IntersectionObserver(entries=>{
+
+    const observer = new IntersectionObserver((entries)=>{
+
         entries.forEach(entry=>{
+
             if(entry.isIntersecting){
+
                 entry.target.classList.add("show");
-            }
-            else{
+
+            }else{
+
                 entry.target.classList.remove("show");
+
             }
+
         });
+
     },{
-        threshold:.15
+        threshold:0,
+        rootMargin:"0px 0px -50px 0px"
     });
-    document
-        .querySelectorAll(
-            "section, footer"
-        )
-        .forEach(section=>{
-            section.classList.add("reveal");
-            observer.observe(section);
-        });
+
+    document.querySelectorAll(`
+        .section-title,
+        .featured-slide,
+        .gallery-item,
+        .resume-preview,
+        .resume-profile,
+        .skill-card,
+        .about-card,
+        .contact-card 
+    `).forEach(element=>{
+
+        element.classList.add("reveal");
+
+        observer.observe(element);
+
+    });
+
 }
